ec55b2c40fa50cb91e68f2011002377203cd8a55
Diptaparna Biswas <diptaparna@gmail.com>
2017-05-06 17:11:28 +0530

    Added AddCut and ApplyCut

--- Build/SampleAnalyzer/User/Analyzer/cms_pp_bbdm.cpp
+++ Build/SampleAnalyzer/User/Analyzer/cms_pp_bbdm.cpp
@@ -14,13 +14,27 @@ bool cms_pp_bbdm::Initialize(const MA5::Configuration& cfg,
 
 	cout << "BEGIN Initialization" << endl;
 	// Declaring all signal regions
+	Manager()->AddRegionSelection("SR1");
+	Manager()->AddRegionSelection("SR2");
 
 	// Declaring preselection cuts
+	Manager()->AddCut("MET>200");
+	Manager()->AddCut("nJets<=3");
+	Manager()->AddCut("nolepton");
 
-	// Other cuts
-
+	// Declaring histograms
 	MET_SR1=new TH1F("MET_SR1","MET for SR1",8,200,1000);
 	MET_SR2=new TH1F("MET_SR2","MET for SR2",8,200,1000);
+
+	// Declaring signal region cuts
+	string SR_1_2[]= {"SR1","SR2"};
+	Manager()->AddCut("nJets<=2","SR1");
+	Manager()->AddCut("pt1stJet>50",SR_1_2);
+	Manager()->AddCut("pt2ndJet>30","SR1");
+	Manager()->AddCut("pt2ndJet>50","SR2");
+	Manager()->AddCut("pt3rdJet>30","SR2");
+	Manager()->AddCut("nBtag=1","SR1");
+	Manager()->AddCut("nBtag=2","SR2");
 	cout << "END Initialization" << endl;
 	return true;
 }
@@ -109,15 +123,18 @@ bool cms_pp_bbdm::Execute(SampleFormat& sample, const EventFormat& event) {
 	// Applying the Cuts
 
 	// Cut: MET > 200
+	Manager()->ApplyCut(MET > 200, "MET>200");
 	if (MET <= 200)
 		return false;
 	// Cut: No of Jets <= 3
+	Manager()->ApplyCut(noOfJets <= 3, "nJets<=3");
 	if (noOfJets > 3)
 		return false;
 	// Cut: At least 1 jet
 	if (noOfJets < 1)
 		return false;
 	// Cut: 1st Jet has pt > 50
+	Manager()->ApplyCut(SignalJets[0]->pt() > 50, "pt1stJet>50");
 	if (SignalJets[0]->pt() <= 50)
 		return false;
 	// Cut: 1st Jet has delta_phi with MET > 0.5
@@ -126,12 +143,16 @@ bool cms_pp_bbdm::Execute(SampleFormat& sample, const EventFormat& event) {
 
 	// Check if the event satisfies SR1 conditions
 	bool SR1 = true;
+	Manager()->ApplyCut(noOfJets <= 2, "nJets<=2");
 	if (noOfJets == 3)
 		SR1 = false;
 	if (noOfJets == 2) {
+		Manager()->ApplyCut(SignalJets[1]->pt() > 30, "pt2ndJet>30");
 		if (SignalJets[1]->pt() <= 30
 				|| SignalJets[1]->dphi_0_pi(pTmiss) <= 0.5)
 			SR1 = false;
+		Manager()->ApplyCut(SignalJets[0]->btag() && SignalJets[1]->btag(),
+				"nBtag=1");
 		if (SignalJets[0]->btag() && SignalJets[1]->btag())
 			SR1 = false;
 	}
@@ -144,6 +165,7 @@ bool cms_pp_bbdm::Execute(SampleFormat& sample, const EventFormat& event) {
 	if (noOfJets < 2)
 		SR2 = false;
 	if (noOfJets == 2) {
+		Manager()->ApplyCut(SignalJets[1]->pt() > 50, "pt2ndJet>50");
 		if (SignalJets[1]->pt() <= 50
 				|| SignalJets[1]->dphi_0_pi(pTmiss) <= 0.5)
 			SR1 = false;
@@ -151,6 +173,7 @@ bool cms_pp_bbdm::Execute(SampleFormat& sample, const EventFormat& event) {
 			SR2 = false;
 	}
 	if (noOfJets == 3) {
+		Manager()->ApplyCut(SignalJets[2]->pt() > 30, "pt3rdJet>30");
 		if (SignalJets[2]->pt() <= 30
 				|| SignalJets[2]->dphi_0_pi(pTmiss) <= 0.5)
 			SR1 = false;
@@ -158,11 +181,13 @@ bool cms_pp_bbdm::Execute(SampleFormat& sample, const EventFormat& event) {
 		for (size_t i = 0; i < 3; i++)
 			if (SignalJets[i]->btag())
 				noBtaggedJets++;
-		if (noOfJets != 2)
+		Manager()->ApplyCut(noBtaggedJets == 2, "nBtag=2");
+		if (noBtaggedJets != 2)
 			SR2 = false;
 	}
 
 	// Cut: No lepton or photon
+	Manager()->ApplyCut(noElectron && noMuon && noTau && noPhoton, "nolepton");
 	if (!(noElectron && noMuon && noTau && noPhoton))
 		return false;
 
@@ -174,3 +199,4 @@ bool cms_pp_bbdm::Execute(SampleFormat& sample, const EventFormat& event) {
 
 	return (SR1 && SR2);
 }
+
